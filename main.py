f7d4d1d6001e0773f7423695abc630fdd5ac9f2c
Matija Domovic <matija.domovic6@gmail.com>
2023-04-06 22:52:00 +0200

    set download location as output_path

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from pytube import Playlist
 import os
 
 playlist_url = os.environ["YT_PLAYLIST_URL"]
+output_path = os.environ["YT_DOWNLOAD_LOC"]
 
 # create Playlist object and give URL of playlist 
 playlist = Playlist(playlist_url)
 
 # print the title of the playlist 
-#print(playlist.title)
+# print(playlist.title)
 
 # print URLs of the videos in the playlist
-for url in playlist.video_urls:
-	print(url)
+# for url in playlist.video_urls:
+# 	print(url)
 
 # download videos with the highest quality
 for video in playlist.videos:
     print('Downloading: ', video.title)
-    video.streams.get_highest_resolution().download()
+    video.streams.get_highest_resolution().download(output_path=output_path)
